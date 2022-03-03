9d8e1e12e7143b77f3ca134ad5ca9b6b1cab8618
annakkili <annakkiligce365@gmail.com>
2022-03-02 18:40:20 -0800

    project created

---
+++ java/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+ReactDom.render( <
+    >
+    <
+    h1 > hi Annakkili < /h1>  <
+    p > welcome zealous < /p> <
+    />, document.getElementById('root')
+)
